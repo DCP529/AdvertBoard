259f8275639e62380e2c90dbf923145d1ef435fd
DCP529 <61003286+DCP529@users.noreply.github.com>
2022-10-28 20:29:42 +0300

    Add models: user and advert.

--- Models/Advert.cs
+++ Models/Advert.cs
@@ -11,8 +11,8 @@ namespace Models
         public Guid? Id { get; set; }
         public int Number { get; set; }
         public Guid UserId { get; set; }
-        public string? Text { get; set; }
-        public string? ImagePath { get; set; }
+        public string Text { get; set; }
+        public string ImagePath { get; set; }
         public int MyProperty { get; set; }
         public int Rating { get; set; }
         public DateTime DateOfCreation { get; set; }
