92ac46ed5f167419b8f6d747f3e0b3ed96474294
DCP529 <61003286+DCP529@users.noreply.github.com>
2022-10-29 19:08:16 +0300

    Add validations, validations tets and fix advert properties.

---
+++ Models/Validation/AdvertValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Validation
+{
+    public class AdvertValidator : AbstractValidator<Advert>
+    {
+        public AdvertValidator()
+        {
+            RuleFor(x => x.Number).NotNull().NotEmpty();
+            RuleFor(x => x.UserId).NotNull().NotEmpty();
+            RuleFor(x => x.Text).NotNull().NotEmpty();
+            RuleFor(x => x.ImagePath).NotNull().NotEmpty();
+            RuleFor(x => x.Rating).NotNull().NotEmpty();
+            RuleFor(x => x.DateOfCreation).NotNull().NotEmpty().InclusiveBetween(DateTime.Parse("01.01.2022"), DateTime.Now);
+            RuleFor(x => x.ExpirationDate).NotNull().NotEmpty();
+        }
+    }
+}
---
+++ ValidationsTests/AdvertValidatorTests.cs
@@ -0,0 +1,61 @@
+﻿using Models;
+using Models.Validation;
+using Xunit;
+
+namespace ValidationsTests
+{
+    public class AdvertValidatorTests
+    {
+        [Fact]
+        public void Properties_AdvertTest()
+        {
+            //Arange
+
+            var validator = new AdvertValidator();
+
+            var advert = new Advert()
+            {
+                Number = 1,
+                UserId = Guid.NewGuid(),
+                Text = "Это новое объявление",
+                ImagePath = @"C:\Users\37377\Desktop\Dex\image.png",
+                Rating = 2,
+                DateOfCreation = DateTime.Parse("22.10.2022")
+            };
+
+            //Act
+
+            var validateResult = validator.Validate(advert);
+
+            //Asset
+
+            Assert.True(validateResult.IsValid);
+        }
+
+        [Fact]
+        public void Properties_Advert_ErrorTest()
+        {
+            //Arange
+
+            var validator = new AdvertValidator();
+
+            var advert = new Advert()
+            {
+                Number = 1,
+                UserId = Guid.Empty,
+                Text = "Это новое объявление",
+                ImagePath = @"C:\Users\37377\Desktop\Dex\image.png",
+                Rating = 2,
+                DateOfCreation = DateTime.Parse("22.10.2022")
+            };
+
+            //Act
+
+            var validateResult = validator.Validate(advert);
+
+            //Asset
+
+            Assert.True(!validateResult.IsValid);
+        }
+    }
+}
