7c2b74cf04c7e593aaad750c93b740cf23b79138
DCP529 <61003286+DCP529@users.noreply.github.com>
2022-10-30 16:44:18 +0200

    Add ModelsDb and create database using migrations

---
+++ Models/ModelsDb/AdvertDb.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ModelsDb
+{
+    [Table(name: "advert")]
+    public class AdvertDb
+    {
+        [Key]
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Column("number")]
+        public int Number { get; set; }
+
+        [Column("user_id")]
+        public Guid UserId { get; set; }
+
+        [Column("text")]
+        public string Text { get; set; }
+
+        [Column("image_path")]
+        public string ImagePath { get; set; }
+
+        [Column("rating")]
+        public int Rating { get; set; }
+
+        [Column("date_of_creation")]
+        public DateTime DateOfCreation { get; set; }
+
+        [Column("expiration_date")]
+        public DateTime ExpirationDate { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        [Column("user")]
+        public UserDb user { get; set; }
+    }
+}
